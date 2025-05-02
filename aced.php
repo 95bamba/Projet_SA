9a7785d3af71c96864595fe36a9fb02a69a17a3a
95bamba <151529639+95bamba@users.noreply.github.com>
2025-05-02 16:29:56 +0000

    Add files via upload

---
+++ aced.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title> Senegal Academy </title>
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Fontawesome CSS -->
+	<link href="css/all.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper-main">
+	<!-- Top Bar -->
+	<div class="top-bar">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="social-media">
+						<ul>
+							<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fas fa-envelope"></i></a></li>
+							<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+							<li><a href="#"><i class="fab fa-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="contact-details">
+						<ul>
+							<li><i class="fas fa-phone fa-rotate-90"></i> +221 33 961 25 20</li>
+							<li><i class="fas fa-map-marker-alt"></i> Rue Hyane Sy, Ndioloféne Saint-Louis, Sénégal</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-light top-nav">
+        <div class="container">
+            <a class="navbar-brand" href="index.html">
+				<img src="images/logoo.png" alt="logo" style="height: 90px; width: 120px;"/>
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fas fa-bars"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="index.html">Accueil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="about.html">A Propos</a>
+					</li>
+					
+					<!--li class="nav-item dropdown">
+						<a class="nav-link" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages <i class="fas fa-sort-down"></i></a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+							<a class="dropdown-item" href="faq.html">FAQ</a>
+							<a class="dropdown-item" href="404.html">404</a>
+							<a class="dropdown-item" href="pricing.html">Pricing Table</a>
+						</div>
+					</li-->
+					<li class="nav-item dropdown">
+						<a class="nav-link active" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Projets <i class="fas fa-sort-down"></i></a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+						    <a class="dropdown-item" href="aea.php">Academy Ecole Aventure</a>
+							<a class="dropdown-item" href="asn.php">Academy Science Numerique</a>
+							<a class="dropdown-item" href="ace.php">Academy Challenge Entreprenariat</a>
+
+							<a class="dropdown-item" href="mwa.php">Meting Women Academy</a>
+							<a class="dropdown-item" href="ac.php">Academy Culture, Environnement, Sport et Santé</a>
+							<a class="dropdown-item" href="aced.php">Academy Concours, Epelle-moi et Débattons</a>
+							<a class="dropdown-item" href="af.php">Academy Formation</a>
+							<a class="dropdown-item" href="ai.php">International Studies Academy</a>
+							<a class="dropdown-item" href="#">SAIN'TIC</a>
+						</div>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="faq.html">FAQ</a>
+					</li>
+					<!--li class="nav-item dropdown">
+						<a class="nav-link" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Blog <i class="fas fa-sort-down"></i></a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+							<a class="dropdown-item" href="blog.html">Blog</a>
+							<a class="dropdown-item" href="blog-post.html">Blog Post</a>
+						</div>
+					</li-->
+					<li class="nav-item">
+						<a class="nav-link" href="contact.html">Contact</a>
+					</li>
+				</ul>
+            </div>
+        </div>
+    </nav>
+	
+	<!-- full Title -->
+	<div class="full-title">
+		<div class="container">
+			<!-- Page Heading/Breadcrumbs -->
+			<h1 class="mt-4 mb-3"> Academy Concour Epelle-moi et Débatons </h1>
+			<div class="breadcrumb-main">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item">
+						<a href="index.html">Accueil</a>
+					</li>
+					<li class="breadcrumb-item active">Portfolio</li>
+				</ol>
+			</div>
+		</div>
+	</div>
+
+    <div class="item-pro">
+		<div class="container">
+			<!-- Portfolio Item Row -->
+			<div class="row">
+				<div class="col-md-8">
+					<img class="img-fluid" src="images/dg.jfif" alt="" />
+				</div>
+				<div class="col-md-4">
+					<h3 class="my-3">Phasellus et nisi ut sapien ultricies laoreet.</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+					<h3 class="my-3">Project info</h3>
+					<ul>
+						<li><span>Project Name :</span><span>Nulla tincidunt justo</span></li>
+						<li><span>Category :</span><span>Vestibulum sagittis</span></li>
+						<li><span>Client :</span><span>Lorem ipsum </span></li>
+						<li><span>Website :</span><span>www.lipsum.com</span></li>
+						<li><span>Date :</span><span>11-10-2018</span></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="mt-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+					<p class="mt-3">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+				</div>
+			</div>
+			
+			<!-- /.row -->
+			<div class="related-projects">
+				<!-- Related Projects Row -->
+				<h3> Projects Réalisés</h3>
+				<div class="row">
+					<div class="col-md-3 col-sm-6 mb-4">
+						<a href="#">
+							<img class="img-fluid" src="images/54.jpg" alt="" />
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 mb-4">
+						<a href="#">
+							<img class="img-fluid" src="images/54.jpg" alt="" />
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 mb-4">
+						<a href="#">
+							<img class="img-fluid" src="images/54.jpg" alt="" />
+						</a>
+					</div>
+					<div class="col-md-3 col-sm-6 mb-4">
+						<a href="#">
+							<img class="img-fluid" src="images/54.jpg" alt="" />
+						</a>
+					</div>
+				</div>
+				<!-- /.row -->
+			</div>
+		</div>
+		<!-- /.container -->
+	</div>
+	
+	<!-- Contact Us -->
+	<!--div class="touch-line">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+				   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, expedita, saepe, vero rerum deleniti beatae veniam harum neque nemo praesentium cum alias asperiores commodi.</p>
+				</div>
+				<div class="col-md-4">
+				   <a class="btn btn-lg btn-secondary btn-block" href="#"> Contact Us </a>
+				</div>
+			</div>
+		</div>
+	</div-->
+
+    <!--footer starts from here-->
+    <footer class="footer" style="background: #011b11; opacity: 80%;">
+        <div class="container bottom_border">
+            <div class="row">
+				<div class="col-lg-4 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">Rue Hyane Sy,Ndioloféne Saint-Louis, Sénégal</h5>
+					<!--headin5_amrc-->
+					<!--p class="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p-->
+					
+				</div>	
+				<div class="col-lg-4 col-md-6 col-sm-6">
+					<h5 class="headin5_amrc col_white_amrc pt2">+221 33 951 25 20</h5>
+					<!--headin5_amrc-->
+					<!--ul class="footer_ul_amrc">
+						<li><a href="#"><i class="fas fa-long-arrow-alt-right"></i>Default Version</a></li>
+						<li><a href="#"><i class="fas fa-long-arrow-alt-right"></i>Boxed Version</a></li>
+						<li><a href="#"><i class="fas fa-long-arrow-alt-right"></i>Our Team </a></li>
+						<li><a href="#"><i class="fas fa-long-arrow-alt-right"></i>About Us</a></li>
+						<li><a href="#"><i class="fas fa-long-arrow-alt-right"></i>Our Services</a></li>
+						<li><a href="#"><i class="fas fa-long-arrow-alt-right"></i>Get In Touch</a></li>
+					</ul-->
+					<!--footer_ul_amrc ends here-->
+				</div>
+				<div class="col-lg-4 col-md-6 col-sm-6 col">
+					<h5 class="headin5_amrc col_white_amrc pt2">contact.senegalacademy@gmail.com</h5>
+					<!--headin5_amrc ends here-->
+					<!--ul class="footer_ul2_amrc">
+						<li>
+							<a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+							<p>Lorem Ipsum is simply dummy...<a href="#">https://www.lipsum.com/</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+							<p>Lorem Ipsum is simply dummy...<a href="#">https://www.lipsum.com/</a></p>
+						</li>
+						<li>
+							<a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
+							<p>Lorem Ipsum is simply dummy...<a href="#">https://www.lipsum.com/</a></p>
+						</li>
+					</ul-->
+					<!--footer_ul2_amrc ends here-->
+				</div>
+				<!--div class="col-lg-3 col-md-6 col-sm-6 ">
+					<div class="news-box">
+						<h5 class="headin5_amrc col_white_amrc pt2">Newsletter</h5>
+						<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+						<form action="#">
+							<div class="input-group">
+								<input class="form-control" placeholder="Search for..." type="text">
+								<span class="input-group-btn">
+								  <button class="btn btn-secondary" type="button">Go!</button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div-->
+			</div>
+		</div>
+        <div class="container">
+            <p class="copyright text-center">All Rights Reserved. &copy; 2018 <a href="#">Senegal Academy</a> Concepté Par : 
+				<a href="https://technofuturgroup.com/">Technofutur Group</a>
+            </p>
+
+			<ul class="footer-social">
+				<li><a class="facebook hb-xs-margin" href="#"><span class="hb hb-xs spin hb-facebook"><i class="fab fa-facebook-f"></i></span></a></li>
+				<li><a class="twitter hb-xs-margin" href="#"><span class="hb hb-xs spin hb-twitter"><i class="fab fa-twitter"></i></span></a></li>
+				<li><a class="instagram hb-xs-margin" href="#"><span class="hb hb-xs spin hb-instagram"><i class="fab fa-instagram"></i></span></a></li>
+				<li><a class="googleplus hb-xs-margin" href="#"><span class="hb hb-xs spin hb-google-plus"><i class="fab fa-google-plus-g"></i></span></a></li>
+				<li><a class="dribbble hb-xs-margin" href="#"><span class="hb hb-xs spin hb-dribbble"><i class="fab fa-dribbble"></i></span></a></li>
+			</ul>
+        </div>
+    </footer>
+
+	
+</div>
+	  
+
+<style>
+	/*!
+ *  Zonebiz - Business Consulting Bootstrap4 Responsive Template
+ */
+
+@import url('https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900');
+ 
+
+ 
+ /* ... [Tout le code CSS existant jusqu'au footer] ... */
+ 
+ /* ==================== */
+ /* FOOTER CORRECTIONS */
+ /* ==================== */
+ .footer { 
+   width:100%; 
+   background: url(../images/footer-bg.jpg) no-repeat center;
+   background-size: cover;
+   min-height:250px; 
+   padding:40px 0px 25px 0px;
+ }
+ 
+ .footer .container.bottom_border {
+   padding-bottom: 30px;
+ }
+ 
+ .footer h5.headin5_amrc {
+   color: #ffffff;
+   font-weight: 600;
+   font-size: 18px;
+   margin-bottom: 20px;
+   padding-bottom: 10px;
+   position: relative;
+   text-align: center;
+ }
+ 
+ .footer h5.headin5_amrc:after {
+   content: "";
+   position: absolute;
+   width: 50px;
+   height: 2px;
+   background: #19ee9c;
+   bottom: 0;
+   left: 50%;
+   transform: translateX(-50%);
+ }
+ 
+ .footer .footer-social {
+   display: flex;
+   justify-content: center;
+   margin: 0 auto;
+   padding: 0;
+   list-style: none;
+   width: 100%;
+ }
+ 
+ .footer .footer-social li {
+   margin: 0 8px;
+ }
+ 
+ .footer .footer-social li a {
+   color: #ffffff;
+   font-size: 16px;
+   transition: all 0.3s ease;
+ }
+ 
+ .footer .footer-social li a:hover {
+   color: #19ee9c;
+ }
+ 
+ .footer .col-lg-4 {
+   margin-bottom: 30px;
+   text-align: center;
+ }
+ 
+ .footer .copyright {
+   margin-top: 20px;
+   padding-top: 20px;
+   border-top: 1px solid rgba(255,255,255,0.1);
+ }
+ 
+ /* Responsive adjustments */
+ @media (max-width: 767px) {
+   .footer .col-lg-4 {
+	 margin-bottom: 25px;
+   }
+   
+   .footer h5.headin5_amrc {
+	 margin-top: 15px;
+   }
+ }
+ 
+ /* ==================== */
+ /* RESTE DU CODE EXISTANT */
+ /* ==================== */
+ 
+ /* ... [Tout le reste du code CSS existant] ... */
+</style>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.appear.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
